2b26eb7ef6e5dde765a23124432653e98ef03424
Samare97 <maxwiduranga@gmail.com>
2020-05-06 13:10:34 +0530

    first

---
+++ PaymentServiceUIs/WebContent/Components/Main.js
@@ -0,0 +1,192 @@
+$(document).ready(function() {
+	if ($("#alertSuccess").text().trim() == "") {
+		$("#alertSuccess").hide();
+	}
+	$("#alertError").hide();
+	refresh();
+
+});
+
+$(document).on("click","#btnSave",function(event) {
+	
+					// Clear alerts---------------------
+					$("#alertSuccess").text("");
+					$("#alertSuccess").hide();
+					$("#alertError").text("");
+					$("#alertError").hide();
+					// Form validation-------------------
+					var status = validateItemForm();
+					if (status != true) {
+						$("#alertError").text(status);
+						$("#alertError").show();
+						return;
+					}
+
+					var formObj = $("#formPayment")
+					var payment = {}
+					
+					payment["nic"] = formObj.find("#nic").val().trim()
+					payment["cardno"] = formObj.find("#cardno").val().trim()
+					payment["amount"] = formObj.find("#amount").val().trim()
+					
+					
+
+					var type = ($("#hidItemIDSave").val() == "") ? "POST"
+							: "PUT";
+					serviceUrl = "http://localhost:8090/PaymentService/PaymentService/Payments/"
+					if (type == "PUT") {
+						serviceUrl = "http://localhost:8090/PaymentService/PaymentService/Payments/"
+							payment["pID"] = $("#hidItemIDSave").val()
+					}
+					
+					$.ajax({
+						url : serviceUrl,
+						type : type,
+						data : JSON.stringify(payment),
+						contentType : "application/json",
+						beforeSend : function(xhr) {
+							xhr.setRequestHeader("Authorization", "Basic "
+									+ btoa("admin" + ":" + "admin"));
+						},
+						complete : function(response, status) {
+							onItemSaveComplete(response.responseText,
+									status);
+						}
+					});
+				});
+
+
+
+
+$(document).on("click",".btnUpdate",function(event) {
+	
+					$("#heading").text("Update Payment");
+					$("#hidItemIDSave").val($(this).closest("tr").find('#hidItemIDUpdate').val());
+					$("#nic").val($(this).closest("tr").find('td:eq(0)').text());
+					$("#cardno").val($(this).closest("tr").find('td:eq(1)').text());
+					$("#amount").val($(this).closest("tr").find('td:eq(2)').text());
+					
+					
+				});
+
+
+
+
+$(document).on("click",".btnRemove",function(event) {
+			
+					var r = confirm("Do you want to delete this record");
+					if (r == true) {
+						
+						serviceUrl = "http://localhost:8090/PaymentService/PaymentService/Payments/"
+							
+							
+						$.ajax({
+							url : serviceUrl,
+							type : "DELETE",
+							data : "{pID: " + $(this).data("itemid") +"}",
+							contentType : "application/json",
+							beforeSend : function(xhr) {
+								xhr.setRequestHeader("Authorization", "Basic "
+										+ btoa("admin" + ":" + "admin"));
+							},
+							complete : function(response, status) {
+								onItemDeleteComplete(response.responseText,
+										status);
+							}
+
+						});
+					}
+				});
+
+
+function validateItemForm() {
+	//  nic
+	if ($("#nic").val().trim() == "") {
+		return "Insert NIC number.";
+	}
+	// Card NO
+	if ($("#cardno").val().trim() == "") {
+		return "Insert Card Number.";
+	}
+	// Amount
+	if ($("#amount").val().trim() == "") {
+		return "Insert Amount.";
+	}
+	
+	
+
+	
+
+	return true;
+}
+
+function onItemSaveComplete(response, status) {
+	if (status == "success") {
+		var resultSet = JSON.parse(response);
+
+		if (resultSet.status.trim() == "success") {
+			
+			$("#alertSuccess").text("Successfully saved.");
+			$("#alertSuccess").show();
+			$("#divItemsGrid").html(resultSet.data);
+			
+		} else if (resultSet.status.trim() == "error") {
+			$("#alertError").text(resultSet.data);
+			$("#alertError").show();
+		}
+
+	} else if (status == "error") {
+		$("#alertError").text("Error while saving.");
+		$("#alertError").show();
+	} else {
+		$("#alertError").text("Unknown error while saving..");
+		$("#alertError").show();
+	}
+
+	$("#hidItemIDSave").val("");
+	$("#formPayment")[0].reset();
+	
+	refresh();
+} 
+
+
+function onItemDeleteComplete(response, status) {
+	if (status == "success") {
+		var resultSet = JSON.parse(response);
+		if (resultSet.status.trim() == "success") {
+			$("#alertSuccess").text("Successfully deleted.");
+			$("#alertSuccess").show();
+			$("#table").html(resultSet.data);
+		} else if (resultSet.status.trim() == "error") {
+			$("#alertError").text(resultSet.data);
+			$("#alertError").show();
+		}
+	} else if (status == "error") {
+		$("#alertError").text("Error while deleting.");
+		$("#alertError").show();
+	} else {
+		$("#alertError").text("Unknown error while deleting..");
+		$("#alertError").show();
+	}
+	refresh()
+}
+
+function refresh() {
+
+	serviceUrl = "http://localhost:8090/PaymentService/PaymentService/Payments/"
+	$.ajax({
+		dataType : 'html',
+		url : serviceUrl,
+		beforeSend : function(xhr) {
+			xhr.setRequestHeader("Authorization", "Basic "
+					+ btoa("admin" + ":" + "admin"));
+		},
+		success : function(data) {
+			
+			$("#table").html(data);
+			
+		
+		}
+	});
+
+}
---
+++ PaymentServiceUIs/WebContent/index.jsp
@@ -0,0 +1,81 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Health Care System</title>
+<link rel="stylesheet" href="Views/bootstrap.min.css">
+<script src="Components/jquery-3.3.1.min.js"></script>
+<script src="Components/Main.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+
+				<h1 id="heading">Insert Payment</h1>
+
+				<form id="formPayment" name="formPayment">
+				
+					NIC Number:<input id="nic" name="nic" type="text"
+						class="form-control form-control-sm"> <br>
+						
+					Card Number: <input id="cardno" name="cardno" type="text"
+						class="form-control form-control-sm"> <br>
+				
+					Amount : <input id="amount" name="amount" type="text"
+						class="form-control form-control-sm"> <br>	
+				
+					
+					<!-- 	
+					Username: <input id="username" name="username" type="text"
+						class="form-control form-control-sm"> <br>
+					
+					Password: <input id="password" name="password" type="password"
+						class="form-control form-control-sm"> <br>
+					
+ -->
+					 <br> <input id="btnSave" name="btnSave" type="button"
+						value="Save" class="btn btn-primary"> <input type="hidden"
+						id="hidItemIDSave" name="hidItemIDSave" value="">
+				</form>
+				<hr>
+				<div id="alertSuccess" class="alert alert-success"></div>
+				<div id="alertError" class="alert alert-danger"></div>
+				<br>
+
+
+
+			</div>
+			<div class="col-md-8">
+
+				<div class="container">
+					<h2>Payments Table</h2>
+					<p>Available Payments In The System</p>
+					<div id="table">
+					
+					
+					</div>
+					<!-- <table class="table table-striped" id="paymentTable">
+						<thead>
+							<tr>
+								<th>NIC Number</th>
+								<th>Card Number</th>
+								<th>Amount</th>
+								
+							</tr>
+						</thead>
+						<tbody>
+							
+						</tbody>
+					</table> -->
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+
+
+</html>
